a69418eb3e1aa030b01ceccfaff620dee8b4f326
poignant-minifier-0983 <davidfano@gmail.com>
2019-07-13 17:53:11 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_day_and_season_to_show.rb
+++ db/migrate/009_add_day_and_season_to_show.rb
@@ -1,6 +1,5 @@
-class AddDayAndSeasonToShow < ActiveRecord::Migration[5.1]
+class AddGenreToShow < ActiveRecord::Migration[5.1]
   def change
-    add_column :shows, :day, :string
-    add_column :shows, :season, :string
+    add_column :shows, :genre, :string
   end
 end
